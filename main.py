c57f8c6deb1338b0768044e1ec4b33bc0e843906
afkdev8 <mr.infinidy@protonmail.com>
2025-11-01 22:39:17 +0100

    Run connect command with kitty

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ class KeywordQueryEventListener(EventListener):
                 session_path = session.get("Path", "No path available")
 
                 # This command will be executed when the user presses Enter
-                connect_command = f'sesh connect "{session_name}"'
+                connect_command = f'kitty -e sesh connect "{session_name}"'
 
                 items.append(
                     ExtensionResultItem(
